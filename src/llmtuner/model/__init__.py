f7dcc04bfe008ef5eb028568747dec32be5440c1
research-anonymous <moore.wods@gmail.com>
2024-04-06 19:20:21 +0200

    update

--- src/llmtuner/model/__init__.py
+++ src/llmtuner/model/__init__.py
@@ -3,3 +3,4 @@
 from llmtuner.model.loader import load_model_and_tokenizer
 from llmtuner.model.parser import get_train_args, get_infer_args, get_eval_args
 from llmtuner.model.utils import dispatch_model, get_modelcard_args, load_valuehead_params
+from .low_rank_linear import LowRankLinear
